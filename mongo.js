382632a659ae53c2f00082300a37bf9f624abd60
Your Name <ppp1244qqq@gmail.com>
2023-01-06 16:51:48 +0800

    Hello

---
+++ mongo.js
@@ -0,0 +1,13 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+ 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("数据库已创建!");
+  var dbase = db.db("local");
+  dbase.createCollection('site', function (err, res) {
+      if (err) throw err;
+      console.log("创建集合!");
+      db.close();
+  });
+});
