b72c64f6ba51d311db5ad8dce58ac2c85ebde59f
john7k7k <ppp1244qqq@gmail.com>
2023-09-06 20:29:35 +0800

    relocal

---
+++ server/routes/api/fish/proc/relocalFish.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports = async (req,res) => {
+   // try{
+      await prisma.fish.update({
+        where: {
+            fishUID: req.body.fishUID
+        },
+        data: {
+            location: req.body.newPool
+        }
+      })
+      res.sendStatus(200)
+   // }catch{ res.sendStatus(500);}
+  }
