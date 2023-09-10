d1e7ba2e4d202c704cb1819afc28b070fd767d52
john7k7k <ppp1244qqq@gmail.com>
2023-09-10 15:46:34 +0800

    readme2

---
+++ server/modules/middleware/verifyDomin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// 自定义中间件来检查请求的来源
+const allowDomainOnly = (req, res, next) => {
+  const allowedDomain = 'aifish.cc'; // 替换为你的域名
+  const requestOrigin = req.headers.origin || req.headers.referer;
+
+  if (requestOrigin === `http://${allowedDomain}` || requestOrigin === `https://${allowedDomain}`) {
+    next();
+  } else {
+    res.status(403).send('Access Forbidden');
+  }
+};
+module.export = allowDomainOnly;
