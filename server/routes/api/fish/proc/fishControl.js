1a28e410aec8ca3d1fd545120148663408a3d650
john7k7k <122036060+john7k7k@users.noreply.github.com>
2023-11-15 21:13:23 +0800

    Update fishControl.js

--- server/routes/api/fish/proc/fishControl.js
+++ server/routes/api/fish/proc/fishControl.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
         
         const { fishControl } = req.body; //取得參數
         const { section } = req.query;
+        /*
         mqttConnection.publish(`Fish/control/${section}/require`, "nothing");
         console.log(`Fish/control/${section}/require`);
         const controlTask = () => {
@@ -20,11 +21,12 @@ module.exports = async (req, res) => {
                 setTimeout(controlTask, 2000);
             }
         }
-        setTimeout(controlTask, 20);
+        setTimeout(controlTask, 20);*/
         for(key in fishControl){
             const fish_string = JSON.stringify(fishControl[key]); //轉換為json字串
             mqttConnection.publish('Fish/control/' +  section.slice(0,3) + section.slice(3,6) +section.slice(6) + '/'  + key,fish_string);
             console.log('Fish/control/' + section.slice(0,3) + section.slice(3,6) +section.slice(6) + '/'  + key);
+            console.log(fish_string);
         }
         res.sendStatus(200);
     }
