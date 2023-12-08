a60c910a4a2437cb2563415b02f59124f64cd705
john <you@example.com>
2023-12-09 00:25:20 +0800

    a

--- server/modules/mqtt/proc/message.js
+++ server/modules/mqtt/proc/message.js
@@ -1,11 +1,9 @@
 const lineNotify = require('../../util/lineNotify')();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const { prisma } =  require('../../util/myPrisma.js') ;
 
 module.exports = async (topic, rec_message) => { //接收到IOT端訊息
-
-        const json_data = JSON.parse(rec_message.toString()); //parse資料
-        messageProcess(topic,json_data);
+    const json_data = JSON.parse(rec_message.toString()); //parse資料
+    messageProcess(topic,json_data);
     
 }
 
@@ -14,10 +12,10 @@ function topicDecode(topic){
     return {
       main: topicArray[0],
       type: topicArray[1],
-      instruction: topicArray[2],
-      depart: topicArray[3],
-      pool: topicArray[4],
-      command: topicArray[5]
+      instruction: topicArray[2].slice(0,3),
+      depart: topicArray[2].slice(3,6),
+      pool: topicArray[2].slice(6),
+      command: topicArray[3]
     }
   }
  
@@ -25,11 +23,13 @@ function topicDecode(topic){
 async function messageProcess(topic,mqtt_data){
     try{
         console.log(topic + ' 收到訊息' + `message: ${JSON.stringify(mqtt_data)}`)
-        if(topic = 'Ota/port'){
+        
+        if(topic === 'Ota/port'){
             global.fishPort = mqtt_data.port;
             return;
         }
         topic = topicDecode(topic);
+        console.log(topic)
         if(topic.type === 'alarm'){
             const { name: instruction, depart: [{name: depart, pool: [{name: pool}]}] } = 
             await prisma.instruction.findUnique({ 
@@ -51,13 +51,11 @@ async function messageProcess(topic,mqtt_data){
         }
         else if(topic.type === 'info'){
             delete mqtt_data.time;
-            console.log(mqtt_data);
             let thisPoolFishesUID = (await prisma.fish.findMany({
                 where: {
                     location: topic.instruction + topic.depart + topic.pool
                 }
             })).map(({ fishUID }) => fishUID)
-            console.log(thisPoolFishesUID)
             Object.keys(mqtt_data).forEach(fishID => {
                 if(!thisPoolFishesUID.map(fishUID => fishUID.slice(3)).includes(fishID)){
                     delete mqtt_data[fishID];
@@ -73,15 +71,15 @@ async function messageProcess(topic,mqtt_data){
                 }
               })
               const now = Math.floor((new Date()).getTime()/1000)
-            
-            const fishesData = fishes.map(({ fishData }) => fishData)
+            const fishesData = fishes.map(({ fishData }) => fishData[0]);
             await prisma.fishData.createMany({
             data: Object.keys(mqtt_data).map(fishID => ({
                     fishUID: '002' + fishID,
                     time: now,
                     ...(mqtt_data[fishID]),
                     accumulationTime:
-                    fishesData[fishesData.findIndex(fish => fish.fishUID === '002' + fishID)].accumulationTime +( now - fishesData[fishesData.findIndex(fish => fish.fishUID === '002' + fishID)].time)
+                    fishesData[fishesData.findIndex(fish => fish.fishUID === '002' + fishID)].accumulationTime + 
+                    (fishesData[fishesData.findIndex(fish => fish.fishUID === '002' + fishID)].active?( now - fishesData[fishesData.findIndex(fish => fish.fishUID === '002' + fishID)].time):0)
                 }))
             })
         }
