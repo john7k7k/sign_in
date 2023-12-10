a277e37a735639f44d1b108d809d5a737ee9758a
john <you@example.com>
2023-12-11 03:32:27 +0800

    add homepage

--- server/routes/page/index.js
+++ server/routes/page/index.js
@@ -5,7 +5,7 @@ const verifyTokenBy = require('../../modules/middleware/verifyToken');
 
 router.get('/', function(req, res) { res.redirect('login'); });
   
-router.get(/^\/(?:login|sign\/up)$/, sendpage);
+router.get(/^\/(?:login|sign\/up|homepage|about|services|contact)$/, sendpage);
 
 router.get(
     /^\/(?:ntut\/fish|nmmst\/fish|ntut\/fish\/edit|nmmst\/fish\/edit|user|home|fish\/list|account\/list)$/, 
