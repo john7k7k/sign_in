103e3d84785a093be62d7563a1ef8243ceae7708
john <you@example.com>
2023-12-12 13:04:01 +0800

    a

--- server/routes/api/fish/proc/fishExplore.js
+++ server/routes/api/fish/proc/fishExplore.js
@@ -2,7 +2,7 @@ const mqttConnection = require('../../../../modules/util/mqtt');
 
 module.exports = async (req, res) => {
     try{
-        mqttConnection.publish('Fish/explore/', JSON.stringify({ explore: true }));
+        mqttConnection.publish(`Fish/explore/${req.query.section}`, JSON.stringify({ explore: true }));
         res.sendStatus(200);
     }catch(e){res.status(500).send(e)}
 }
