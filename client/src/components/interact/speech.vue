d8d8b76aba566dacd2cecf85116f15ff99bd4e42
name <you@example.com>
2024-05-22 16:19:31 +0800

    speechNew

--- client/src/components/interact/speech.vue
+++ client/src/components/interact/speech.vue
@@ -1,6 +1,19 @@
 <template>
+    <div class="configureBackdrop" v-show="showBackdrop">
+        <div class="startBox">
+            <v-btn   @click="changeLangWord" variant="flat" color="blue-darken-1" >繁中/En</v-btn>
+            <v-img src="../../assets/Fishbowl.png"  class="startImg"></v-img>
+            <div class="startWordCSS">
+                <p>{{ starttitalWord[languageIndex][0] }}</p>
+                <p>{{ starttitalWord[languageIndex][1] }}</p>
+            </div>
+            <v-btn class="startBtnCSS" @click="showBackdrop = false" variant="flat" color="teal-lighten-2"  >{{ startBtnWord[languageIndex] }}</v-btn>
+        </div>
+    </div>
     <div class="box">
-        <div class="tital">語音控制</div>
+        <v-btn variant="text" class="changeLan" @click="changeLangWord">繁中/En</v-btn>
+        <div class="tital">{{ titalWord[languageIndex] }}</div>
+        
     <div>
       <label for="dropdown" style="color: white;" v-show="false">選擇控制魚的ID:</label>
       <select id="dropdown" v-model="selectedfishUID" @change="handleChange">
@@ -8,28 +21,74 @@
       </select>
     </div>
     <label for="languageSelect" style="color: white;" v-show="false">選擇語言:</label>
-    <select id="languageSelect" v-model="selectedLanguage" @change="changeLanguage" style="color: white;" v-show="false">
+    <select id="languageSelect" v-model="selectedLanguage" @change="changeLanguage" class="changeLanCSS" v-show="false">
       <option value="en-US" style="color: white;">英文</option>
       <option value="zh-CN" style="color: white;">中文</option>
       <!-- 其他語言選項 -->
     </select>
     <br/>
     
-    <p  class="resultTital">辨識結果</p>
+    <p  class="resultTital">{{ resultWord[languageIndex] }}</p>
     <v-card class="resultcard"><div class="Resultsword">{{ command }}</div></v-card>
     <v-btn class="btn-bg text-white" @click="toggleSpeechRecognition"  :icon="icon" size="80"></v-btn>
-    <div v-show="!isListening" style="color: white;" class="beginWord">開始辨識</div>
-    <div v-show="isListening" style="color: white;" class="beginWord">結束辨識</div>
+    <div v-show="!isListening" style="color: white;" class="beginWord">{{ startWord[languageIndex] }}</div>
+    <div v-show="isListening" style="color: white;" class="beginWord">{{ endWord[languageIndex] }}</div>
     </div>
 </template>
 <style scoped>
+.configureBackdrop {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); 
+  z-index: 1; 
+}
+.startBox{
+    width: 35%;
+    height: 75%;
+    background-color: white;
+    border-radius: 20px;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
+
+.startImg{
+    margin: auto;
+    width: 65%;
+}
+.startWordCSS{
+    width: 100%;
+    height: 35%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    font-weight: bolder;
+}
+.startBtnCSS{
+    height: 10%;
+    width: 80%;
+    font-size: 25px;
+    margin: auto;
+}
 .box{
-    width: 90%;
+    width: 95%;
     height: 100%;
     display: flex;
     flex-direction: column;
     margin: auto;
     align-items: center;
+}
+.changeLanCSS{
+    color: white;
 }
 
 @media screen and  (min-width: 1681px){
@@ -82,6 +141,14 @@
     font-size: 45px;
     color: white;
 }
+.changeLan{
+    color: white;
+    font-size: 18px;
+    font-weight: bold;
+    position: fixed;
+    right: 1%;
+    top: 11.5%;
+}
 .resultTital{
     color: white;
     font-size: 25px;
@@ -119,6 +186,16 @@
 }
 }
 @media screen and  (min-width: 601px) and (max-width: 1024px){
+.startBox{
+    width: 60%;
+    height: 65%;
+    background-color: white;
+    border-radius: 20px;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
     .tital{
     font-weight: bold;
     margin-top: 18%;
@@ -130,6 +207,7 @@
     font-size: 30px;
     margin-top: 10%;
 }
+
 .resultcard{
     width: 75%;
     height: 30%;
@@ -163,6 +241,42 @@
 }
 }
 @media screen and (min-width: 401px) and (max-width: 600px){
+.startBox{
+    width: 75%;
+    height: 60%;
+    background-color: white;
+    border-radius: 20px;
+    padding: 4%;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
+.startImg{
+    margin: auto;
+    width: 65%;
+    position: relative;
+    bottom: 5%;
+}
+.startWordCSS{
+    width: 100%;
+    height: 20%;
+    position: relative;
+    bottom: 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bolder;
+}
+.startBtnCSS{
+    height: 10%;
+    width: 80%;
+    font-size: 25px;
+    margin: auto;
+    position: relative;
+    bottom: 5%;
+}
     .tital{
     font-weight: bold;
     margin-top: 30%;
@@ -208,6 +322,42 @@
 }
 }
 @media screen and (max-width: 400px) {
+    .startBox{
+    width: 75%;
+    height: 60%;
+    background-color: white;
+    border-radius: 20px;
+    padding: 4%;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
+.startImg{
+    margin: auto;
+    width: 65%;
+    position: relative;
+    bottom: 5%;
+}
+.startWordCSS{
+    width: 100%;
+    height: 20%;
+    position: relative;
+    bottom: 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bolder;
+}
+.startBtnCSS{
+    height: 10%;
+    width: 80%;
+    font-size: 25px;
+    margin: auto;
+    position: relative;
+    bottom: 5%;
+}
     .tital{
     font-weight: bold;
     margin-top: 30%;
@@ -261,6 +411,9 @@ import axios from 'axios';
 export default {
     data() {
         return {
+            showBackdrop:true,
+            starttitalWord:[["體驗語音操控的樂趣","讓你的聲音引導機器魚游動！"],["Start experiencing your voice","guiding the robotic fish to swim!"]],
+            startBtnWord:["開始體驗","Start"],
             selectedfishUID: null,
             options: [
                 { value: 'option1', label: '選項 1' },
@@ -268,18 +421,25 @@ export default {
                 { value: 'option3', label: '選項 3' }
             ],
             recognition: null,
-            selectedLanguage: 'zh-CN', // 預設語言為中文
+            selectedLanguage: ['zh-CN',"en-US"], // 預設語言為中文
             recognitionResult: '',
             command:"",
             isListening:false,
             icon : "mdi mdi-microphone",
+            English: "en-US",
+            languageIndex: 0,
+            titalWord:["語音控制","Voice Control"],
+            resultWord:["辨識結果","Identification results"],
+            startWord: ["開始辨識","Start recognition"],
+            endWord: ["結束辨識","End recognition"],
+            commandWord: ["辨識中","Identifying"],
         };
     },
     mounted() {
         // 初始化語音辨
         this.fetchOptions();
         this.recognition = new window.webkitSpeechRecognition();
-        this.recognition.lang = this.selectedLanguage; // 使用使用者選擇的語言
+        this.recognition.lang = this.selectedLanguage[this.languageIndex]; // 使用使用者選擇的語言
 
         // 設定辨識事件的處理函數
         this.recognition.onresult = event => {
@@ -305,8 +465,8 @@ export default {
         
         // 開始語音辨識
         this.recognition.start();
-            this.recognitionResult = "辨識中";
-            this.command = "辨識中";
+            this.recognitionResult = this.commandWord[this.languageIndex];
+            this.command = this.commandWord[this.languageIndex];
         },
         endSpeechRecognition(){
             this.isListening = false;
@@ -366,6 +526,11 @@ export default {
             var flattenedObject = {};
             this.traverseAndFlatten(obj, flattenedObject);
             return flattenedObject;
+        },
+        changeLangWord(){
+          if(this.languageIndex == 0){
+            this.languageIndex = 1;
+          }else this.languageIndex = 0;
         }
     }
 };
