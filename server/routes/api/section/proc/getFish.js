58fc1470782099976458d561860f94a5c313450a
Your Name <you@example.com>
2024-06-14 01:10:29 +0800

    fish wiat debug 2

--- server/routes/api/section/proc/getFish.js
+++ server/routes/api/section/proc/getFish.js
@@ -32,8 +32,7 @@ async function getNonFaultyFishInPond(location, res) {
         const dir = path.join(__dirname, `../../../../uploads/photos/fish/${fishUID}`);
         console.log(dir)
         fs.readdir(dir , (err, photos) => {
-            console.log(photos)
-            fishPhotoName[fishUID] = photos.at(-1)?path.join(dir, photos.at(-1)):'no image'
+            if(photos) fishPhotoName[fishUID] = photos.at(-1)?path.join(dir, photos.at(-1)):'no image';
             console.log(nonFaultyFish)
             if(fishUID === nonFaultyFish.at(-1).fishUID) return res.send(fishPhotoName);
         })
