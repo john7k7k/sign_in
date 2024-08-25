ee4cc0ff5583627d9ec08b22dedc1196b378ed3e
Your Name <you@example.com>
2024-08-25 22:16:10 +0800

    a

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -7,7 +7,7 @@ const section = require('./section');
 const ota = require('./ota');
 const controller = require('./controller');
 const component = require('./component');
-
+const stream = require('./stream');
 router.use(`/account`, account)
    
 router.use(`/fish`, fish)
@@ -19,5 +19,7 @@ router.use('/ota', ota)
 router.use('/controller', controller)
 
 router.use('/component', component)
+
+router.use('/stream', stream)
 
 module.exports = router;
