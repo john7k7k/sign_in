cc51dfcc6892b8210d9a624bd1e8cd760818ffb5
name <you@example.com>
2024-08-29 15:40:10 +0800

    new

--- server/config/voiceKeyword.js
+++ server/config/voiceKeyword.js
@@ -10,8 +10,8 @@ const instruction = {
 
 const chineseKeyword = {
   "O":['前', '進', '走','田','恬','天'],
-  "L":['左','佐','都','周'],
-  "R":['右','又','佑','修','幼','柚'],
+  "L2":['左','佐','都','周'],
+  "R2":['右','又','佑','修','幼','柚'],
   "U":['上','水面','抬','浮','尚','尙','站'],
   "D":['下', '底','夏','嚇','厦','加'],
   "X":['停','廷','庭','亭','靜','定','紫', '仔','紙','紙','致'],
@@ -20,8 +20,8 @@ const chineseKeyword = {
 const englishKeyword = {
   "X":['stop','star','set'],
   "O":['forward', 'howard', 'ward', 'food', 'word', 'world','wilson','wood',' worth','go','full','water','cold','call','for','what','bored'],
-  "L":['left', 'let', 'laft', 'last', 'best', 'live', 'plate','life','late','next','laugh','lex','lanter','later'],
-  "R":['right', 'rice', 'bright','@','rise','light','writer'],
+  "L2":['left', 'let', 'laft', 'last', 'best', 'live', 'plate','life','late','next','laugh','lex','lanter','later'],
+  "R2":['right', 'rice', 'bright','@','rise','light','writer'],
   "U":['up', 'app', 'apple', 'pop','opera'],
   "D":['down','tong','dumb','done','dou','no','dial','tell','de','song'],
   "M":['balance','poise', 'ballet', 'alan', 'blanc', 'silence', 'black','paris','pay','bad','palace']
