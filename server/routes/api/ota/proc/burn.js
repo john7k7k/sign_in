ee92329f16e7db47fd541546b777a7e54f71f98d
dingliang <wang1050119@gmail.com>
2024-11-13 21:43:23 +0800

    1113

--- server/routes/api/ota/proc/burn.js
+++ server/routes/api/ota/proc/burn.js
@@ -37,52 +37,65 @@ const awaitMqtt = (req, res) => {
 }
 
 const execute = async (req, res, next) => {
-  if (!req.body.fishesUID[0]) return res.status(403).send('請選擇一條魚')
+  if (!req.body.fishesUID[0]) return res.status(403).send('請選擇一條魚');
   const otaBinName = path.join(__dirname, `sh/flash/aifi_core_stm32_MPU_loopTest_20230920.bin`);
-  await fs.copyFile(path.join(__dirname, `../../../../uploads/ota/${req.body.time + '_' + req.body.version}`), otaBinName, () => {
-  })
-  fs.rename(path.join(__dirname, `../../../../uploads/ota/${req.time + '_' + req.body.version}`), otaBinName, async () => {
-    const options = {
-      cwd: path.join(__dirname, '/sh', '/flash')
-    };
-    const apiUrl = 'https://frp.aifishf.cc/api/proxy/tcp';
-    let ports = 0;
-    let data = 0;
-    await axios.get(apiUrl)
-      .then(response => {
-        // 處理回應數據
-        console.log('成功取得資料:', response.data);
-        data = response.data
-        //console.log(data.proxies[0].conf);
-        fishFrp = data.proxies.find(fishesFrp => fishesFrp.name === `ssh-fish-${req.body.fishesUID[0].slice(3)}-pizero`);
-        if (!fishFrp) return res.status(503).send(`沒有${req.body.fishesUID}`)
-        console.log(fishFrp)
-        if (!fishFrp.conf) return res.status(503).send(`${req.body.fishesUID}不在線`)
-        ports = fishFrp.conf.remote_port
-
-        const bash = `remote_flash.sh 4.216.216.249 ${ports}`; //d:\ntut\mf\webn\web\server\script\fish-eyes.exe get -h
-        //const bash = `test.sh`
-        console.log(bash)
-        console.log(`start flash`);
-        exec(bash, options, (err, stdout, stderr) => {
-          if (err) { console.log("錯誤" + err) }
-          console.log('輸出' + stdout);
-          console.log('輸' + stderr);
-          next();
-        })
-      })
-      .catch(error => {
-        // 處理錯誤
-        const topic = `Ota/fishid`;
-        const message = { id: req.body.fishesUID[0].slice(3) }
-        mqttConnection.publish(topic, JSON.stringify(message));
-        console.log(`publiced: ${topic}, message: ${JSON.stringify(message)}`);
-        global.awaitMqttTime = 0;
-        delete global.fishPort;
-        awaitMqtt(req, res);
+  try {
+    await new Promise((resolve, reject) => {
+      fs.copyFile(path.join(__dirname, `../../../../uploads/ota/${req.body.time + '_' + req.body.version}`), otaBinName, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
       });
-  })
+    });
+    console.log('bin檔成功複製');
+  } catch (err) {
+    console.error(`複製檔案失敗，錯誤: ${err.message}`);
+    return res.status(500).send('無此bin檔');
+  }
+
+  const apiUrl = 'https://frp.aifishf.cc/api/proxy/tcp';
+  let ports = 0;
+  let data = 0;
+
+  await axios.get(apiUrl)
+    .then(response => {
+      // 處理回應數據
+      console.log('成功取得資料:', response.data);
+      data = response.data;
+
+      const fishFrp = data.proxies.find(fishesFrp => fishesFrp.name === `ssh-fish-${req.body.fishesUID[0].slice(3)}-pizero`);
+      if (!fishFrp) return res.status(503).send(`沒有${req.body.fishesUID}`);
+      if (!fishFrp.conf) return res.status(503).send(`${req.body.fishesUID}不在線`);
+
+      ports = fishFrp.conf.remote_port;
+
+      const bash = `remote_flash.sh 4.216.216.249 ${ports}`;
+      console.log(bash);
+      const options = { cwd: path.join(__dirname, '/sh', '/flash') };
+      exec(bash, options, (err, stdout, stderr) => {
+        if (err) {
+          console.log("錯誤:", err);
+          return res.status(500).send("執行升級指令失敗");
+        }
+        console.log('輸出:', stdout);
+        console.log('錯誤輸出:', stderr);
+        next();
+      });
+    })
+    .catch(error => {
+      const topic = `Ota/fishid`;
+      const message = { id: req.body.fishesUID[0].slice(3) };
+      console.log(message);
+      mqttConnection.publish(topic, JSON.stringify(message));
+      console.log(`已發布: ${topic}, 消息: ${JSON.stringify(message)}`);
+      global.awaitMqttTime = 0;
+      delete global.fishPort;
+      awaitMqtt(req, res);
+    });
 }
+
 
 const process = async (req, res) => {
   await prisma.fish.updateMany({
