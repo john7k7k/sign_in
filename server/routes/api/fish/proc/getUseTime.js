1e5ec86b15ba2de5d2a5f74dd80883f8b2585ad6
Your Name <you@example.com>
2024-12-06 23:32:08 +0800

    a

--- server/routes/api/fish/proc/getUseTime.js
+++ server/routes/api/fish/proc/getUseTime.js
@@ -1,11 +1,10 @@
 module.exports = async function getUseTime(req, res){
-    const { times } = req.query;
-    if(!times) res.send(1217);
-    // const data = (await prisma.pool.findUnique({
-    //     where: {
-    //         id: poolID
-    //     }
-    // })).mac
-    //console.log(data)
-    else res.send(times);
+    const poolID = req.query.poolID
+    const data = (await prisma.pool.findUnique({
+        where: {
+            id: poolID
+        }
+    })).mac
+    console.log(data)
+    res.send(data)
 }
